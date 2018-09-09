0893bc973e930b93155ad2f32a1658753bf6eb61
Chad Gray <chad@thegrommet.com>
2018-09-09 10:05:39 -0400

    initial commit

---
+++ clash_royale_api.py
@@ -0,0 +1,45 @@
+#!usr/bin/Python3
+
+from pprint import pprint
+import requests
+import urllib.parse
+import pandas as pd
+
+urlparse = urllib.parse.quote_plus
+df = pd.DataFrame
+
+def toCSV(data, name):
+    f = df(data)
+    file_name = name + '.csv'
+    f.to_csv(file_name, index=False)
+
+import clashroyale_const
+token = clashroyale_const.token
+clan_tag = urlparse(clashroyale_const.elite_clan_tag)
+headers = {'Authorization': "Bearer:" + token}
+base_url = 'https://api.clashroyale.com/v1/'
+
+s = requests.Session()
+
+# setup clan data
+clan_url = base_url + 'clans/' + clan_tag
+r = requests.get(clan_url, headers=headers)
+clan_data = r.json()
+members = clan_data['memberList']
+w = requests.get(clan_url + '/warlog', headers=headers)
+warlog_data = w.json()
+toCSV(warlog_data['items'], 'warlog')
+
+exit()
+players_url = base_url + "players/"
+for player in members:
+    tag = urlparse(player['tag'])
+    p_response = requests.get(players_url + tag, headers=headers)
+    player_data = p_response.json()
+    pprint(player_data)
+    exit()
+    card_data = player_data['cards']
+
+    # battle data
+    b_response = requests.get(players_url + tag + '/battlelog', headers=headers)
+    battle_data = b_response.json()
